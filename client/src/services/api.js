638a27bff23f0c735af20bec468b7e378b76df85
Emilbobin7 <emilbobin965@gmail.com>
2026-02-19 09:01:27 +0530

    fixed api baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://scout-arena-api.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
-    if (token) req.headers.Authorization = token;
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`;
+    }
     return req;
 });
 
